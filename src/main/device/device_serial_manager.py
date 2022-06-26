7aa96d936360c1f6fa240c1f88067000757b5cd1
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-06-26 17:02:41 +0100

    Message handler infrastructure

--- src/main/device/device_serial_manager.py
+++ src/main/device/device_serial_manager.py
@@ -5,17 +5,44 @@ Module that handles serial communication on the device end. Deals with setting u
 messages.
 """
 
+from typing import Callable, TypeVar
 from machine import UART
 import message_protocol
 
 ### Globals ###
 uart = UART(0, message_protocol.BAUD_RATE) # TODO: Potentially move this to init
 
+MESSAGE_HANDLERS = {} # Dictionary mapping message types to handler functions
+T = TypeVar("T", bound = message_protocol.Message)
+
+
+### Functions ###
+
+def register_handler(message_type: type[T], handler: Callable[[T], None]):
+    """
+    Registers the given handler function for the given message type.
+    This will replace any existing handler for that message type.
+    """
+    MESSAGE_HANDLERS[message_type] = handler
+
+
+def test_handler(msg: message_protocol.VolumeMessage):
+    print(msg)
+
+
 def init():
     """
     Initialises the serial manager.
     """
-    pass # Nothing here yet
+    register_handler(message_protocol.VolumeMessage, lambda msg: print(msg.volume))
+    register_handler(message_protocol.VolumeMessage, test_handler)
+
+
+def send(msg: message_protocol.Message):
+    """
+    Sends the given message to the host.
+    """
+    uart.write(msg.encode())
 
 
 def update():
@@ -39,7 +66,7 @@ def update():
             if not data_bytes: raise_msg_error(msg, 0)
             if len(data_bytes) < msg.size: raise_msg_error(msg, len(data_bytes))
             msg.decode(data_bytes)
-            # Do something with the message
+            MESSAGE_HANDLERS[type(msg)](msg) # Call the handler for this type of message to do whatever it needs to do
 
 
 def raise_msg_error(msg, received):
