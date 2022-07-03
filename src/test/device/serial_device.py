33d0489e5d9f7250c825a500210b93254c9b2188
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-07-03 16:01:12 +0100

    Test using stdin with uselect

--- src/test/device/serial_device.py
+++ src/test/device/serial_device.py
@@ -1,5 +1,6 @@
 import sys
 import utime
+import uselect
 from machine import Pin
 
 ONBOARD_LED_PIN = 25
@@ -7,10 +8,21 @@ ONBOARD_LED_PIN = 25
 led_pin = Pin(ONBOARD_LED_PIN, Pin.OUT)
 led_pin.high() # Good check to see whether the script started successfully
 
+stdin_poll = uselect.poll()
+# Not sure why pylance doesn't like the following line
+stdin_poll.register(sys.stdin, uselect.POLLIN)  # type: ignore
+
+def read():
+    return sys.stdin.readline() if stdin_poll.poll(0) else None
+
 while True:
-    ch = sys.stdin.read(4)
-    if str(ch) == 'Ping':
+    ch = read() # sys.stdin.readline()
+    if ch and 'Ping' in str(ch):
         led_pin.toggle()
         sys.stdout.write(b'Pong')
     #sys.stdout.write(ch) # Send it back
-    utime.sleep_ms(20)
+    utime.sleep_ms(200)
+    led_pin.toggle()
+    utime.sleep_ms(200)
+    led_pin.toggle()
+    utime.sleep(1)
