a66ab0af7ad11600021eae8ebf238d49f342bef0
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-08-14 14:56:45 +0100

    Capture messy debug code before tidying up dsm

--- src/main/device/device_serial_manager.py
+++ src/main/device/device_serial_manager.py
@@ -5,13 +5,16 @@ Module that handles serial communication on the device end. Deals with setting u
 messages.
 """
 
-from machine import UART
+import sys
+import utime
+import uselect
+
 import message_protocol
 
 ### Globals ###
-uart = UART(0, message_protocol.BAUD_RATE) # TODO: Potentially move this to init
-
 MESSAGE_HANDLERS = {} # Dictionary mapping message types to handler functions
+
+stdin_poll = uselect.poll()
 
 
 ### Functions ###
@@ -32,40 +35,78 @@ def init():
     """
     Initialises the serial manager.
     """
-    register_handler(message_protocol.VolumeMessage, lambda msg: print(msg.volume))
-    register_handler(message_protocol.VolumeMessage, test_handler)
+    # Not sure why pylance doesn't like the following line
+    stdin_poll.register(sys.stdin.buffer, uselect.POLLIN)  # type: ignore
+    #register_handler(message_protocol.VolumeMessage, lambda msg: sys.stdout.buffer.write(msg.volume))
+    #register_handler(message_protocol.VolumeMessage, test_handler)
 
 
 def send(msg: message_protocol.Message):
     """
     Sends the given message to the host.
     """
-    uart.write(msg.encode())
+    sys.stdout.buffer.write(msg.encode()) # type: ignore
+
+    
+def read(n: int):
+    return sys.stdin.buffer.read(n) if stdin_poll.poll(0) else None # type: ignore
 
 
-def update():
+def update(ring):
     """
     Called from the device controller to update the serial manager.
     """
 
-    while True:
+    #while True:
+    
+    debug_led(ring, (45, 255, 255)) # Orange
 
-        waiting = uart.any() # Get number of bytes waiting in the buffer
-        if not waiting: break # No bytes left to process so we're done here
+    # waiting = stdin_poll.poll(0) # Poll stdin to check if any bytes have been received
+    
+    # debug_led(ring, (240, 255, 255)) # Blue
+    
+    # if not waiting: return # No bytes left to process so we're done here
+    
+    # debug_led(ring, (150, 255, 255)) # Green
 
-        # Reconstruct message object
-        id = uart.read(1) # Unlike sys.stdin.read(), uart.read() doesn't block
-        assert id # Keeps pylance happy
-        msg = message_protocol.msg_from_id(id)
+    # # Reconstruct message object
+    # id = sys.stdin.buffer.read(1) # Blocking, but we know there are bytes waiting so it'll return immediately
 
-        # Decode message
-        if msg.size > 0:
-            data_bytes = uart.read(msg.size)
-            if not data_bytes: raise_msg_error(msg, 0)
-            if len(data_bytes) < msg.size: raise_msg_error(msg, len(data_bytes))
-            msg.decode(data_bytes)
+    id = sys.stdin.buffer.read(1) # Try to read a byte from the input
+
+    debug_led(ring, (270, 255, 255)) # Purple
+
+    utime.sleep(0.5)
+
+    if not id: return # If there are no bytes waiting, we're done
+    ring.display_bytes(id)
+    
+    utime.sleep(1)
+
+    msg = message_protocol.msg_from_id(id)
+    
+    debug_led(ring, (150, 255, 255)) # Green
+
+    utime.sleep(0.5)
+
+    if not msg: return
+
+    # Decode message
+    if msg.size > 0:
+        data_bytes = sys.stdin.buffer.read(msg.size)
+        if not data_bytes: raise_msg_error(msg, 0)
+        if len(data_bytes) < msg.size: raise_msg_error(msg, len(data_bytes))
+        # ring.display_bytes(data_bytes)
+        # sys.stdout.buffer.write(data_bytes)
+        # utime.sleep(5)
+        msg.decode(data_bytes)
+        if type(msg) in MESSAGE_HANDLERS:
             MESSAGE_HANDLERS[type(msg)](msg) # Call the handler for this type of message to do whatever it needs to do
 
 
 def raise_msg_error(msg, received):
-    raise IOError(f"Incomplete message: expected {msg.size} byte(s), received {received} byte(s). Message type: {type(msg)}")
+    raise IndexError(f"Incomplete message: expected {msg.size} byte(s), received {received} byte(s). Message type: {type(msg)}")
+
+def debug_led(ring, clr):
+    ring.set_colour(clr)
+    utime.sleep(1)
