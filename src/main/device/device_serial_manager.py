402388a264b15146bd5d173d494b8cb703691813
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-08-14 15:16:04 +0100

    Tidy up dsm and add support for multiple messages per update cycle

--- src/main/device/device_serial_manager.py
+++ src/main/device/device_serial_manager.py
@@ -12,6 +12,7 @@ import uselect
 import message_protocol
 
 ### Globals ###
+
 MESSAGE_HANDLERS = {} # Dictionary mapping message types to handler functions
 
 stdin_poll = uselect.poll()
@@ -27,18 +28,12 @@ def register_handler(message_type: type, handler):
     MESSAGE_HANDLERS[message_type] = handler
 
 
-def test_handler(msg: message_protocol.VolumeMessage):
-    print(msg)
-
-
 def init():
     """
     Initialises the serial manager.
     """
     # Not sure why pylance doesn't like the following line
     stdin_poll.register(sys.stdin.buffer, uselect.POLLIN)  # type: ignore
-    #register_handler(message_protocol.VolumeMessage, lambda msg: sys.stdout.buffer.write(msg.volume))
-    #register_handler(message_protocol.VolumeMessage, test_handler)
 
 
 def send(msg: message_protocol.Message):
@@ -49,6 +44,12 @@ def send(msg: message_protocol.Message):
 
     
 def read(n: int):
+    """
+    Attempts to read and return n bytes from the serial input buffer. Returns None if no bytes are available.
+    """
+    # TODO: Check how this responds if the number of bytes available is not zero but is less than n
+    #       In theory we shouldn't encounter this problem since we only read more than 1 byte when we know there is a
+    #       message there (and how many bytes it should contain), but for robustness it would be good to nail this down
     return sys.stdin.buffer.read(n) if stdin_poll.poll(0) else None # type: ignore
 
 
@@ -56,49 +57,19 @@ def update(ring):
     """
     Called from the device controller to update the serial manager.
     """
+    while True: # Keep reading messages until there are none waiting
 
-    #while True:
-    
-    debug_led(ring, (45, 255, 255)) # Orange
+        id = read(1) # Try to read a byte from the input
+        if not id: return # If there are no bytes waiting, we're done
 
-    # waiting = stdin_poll.poll(0) # Poll stdin to check if any bytes have been received
-    
-    # debug_led(ring, (240, 255, 255)) # Blue
-    
-    # if not waiting: return # No bytes left to process so we're done here
-    
-    # debug_led(ring, (150, 255, 255)) # Green
+        # Reconstruct message object
+        msg = message_protocol.msg_from_id(id)
+        if msg.size == 0: continue # Some messages contain no additional data - if so, move onto the next message
 
-    # # Reconstruct message object
-    # id = sys.stdin.buffer.read(1) # Blocking, but we know there are bytes waiting so it'll return immediately
-
-    id = sys.stdin.buffer.read(1) # Try to read a byte from the input
-
-    debug_led(ring, (270, 255, 255)) # Purple
-
-    utime.sleep(0.5)
-
-    if not id: return # If there are no bytes waiting, we're done
-    ring.display_bytes(id)
-    
-    utime.sleep(1)
-
-    msg = message_protocol.msg_from_id(id)
-    
-    debug_led(ring, (150, 255, 255)) # Green
-
-    utime.sleep(0.5)
-
-    if not msg: return
-
-    # Decode message
-    if msg.size > 0:
-        data_bytes = sys.stdin.buffer.read(msg.size)
+        # Decode additional message data
+        data_bytes = read(msg.size)
         if not data_bytes: raise_msg_error(msg, 0)
         if len(data_bytes) < msg.size: raise_msg_error(msg, len(data_bytes))
-        # ring.display_bytes(data_bytes)
-        # sys.stdout.buffer.write(data_bytes)
-        # utime.sleep(5)
         msg.decode(data_bytes)
         if type(msg) in MESSAGE_HANDLERS:
             MESSAGE_HANDLERS[type(msg)](msg) # Call the handler for this type of message to do whatever it needs to do
@@ -106,6 +77,7 @@ def update(ring):
 
 def raise_msg_error(msg, received):
     raise IndexError(f"Incomplete message: expected {msg.size} byte(s), received {received} byte(s). Message type: {type(msg)}")
+
 
 def debug_led(ring, clr):
     ring.set_colour(clr)
