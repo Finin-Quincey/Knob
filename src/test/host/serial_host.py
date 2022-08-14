44be94d210ecd456d73430c060d0db174b0b944e
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-08-14 14:56:08 +0100

    Switch serial_host test script over to using the message protocol system

--- src/test/host/serial_host.py
+++ src/test/host/serial_host.py
@@ -10,29 +10,36 @@ COM_PORT = "COM8"
 # Timeout is in seconds
 ser = serial.Serial(COM_PORT, BAUD_RATE, timeout = 5)#, parity = serial.PARITY_EVEN, rtscts = 1)
 
-#msg = msp.VolumeMessage(volume = 3)
-#n = ser.write(msg.encode())
+msg = msp.VolumeMessage(volume = 213)
+n = ser.write(msg.encode())
 
+# This essentially allows you to debug the serial comms!
+# (ampy run won't work for debugging serial since it requires full use of the serial port itself)
 while(True):
+    b = ser.readline()
+    print(b)
+    if not b: break
 
-    try:
-        #msg = bytes([int(input("Enter number: "))])
-        #msg = b'Ping\n'
-        msg = bytes([1, 2, 3])# + b"\n"
-        #msg = [0b10001010, 0b00100001, 0b00101101]
-        n = ser.write(msg)#str(msg, "utf-16"))
-        print(f"Sent {n} bytes: {str(msg)}")
-    except(EOFError):
-        break
+# while(True):
 
-    time.sleep(0.1)
+#     try:
+#         msg = bytes([int(input("Enter number: "))])
+#         #msg = b'Ping\n'
+#         #msg = bytes([1, 2, 3])# + b"\n"
+#         #msg = [0b10001010, 0b00100001, 0b00101101]
+#         n = ser.write(msg)#str(msg, "utf-16"))
+#         print(f"Sent {n} bytes: {str(msg)}")
+#     except(EOFError):
+#         break
 
-    # This essentially allows you to debug the serial comms!
-    # (ampy run won't work for debugging serial since it requires full use of the serial port itself)
-    while(True):
-        b = ser.readline()
-        print(b)
-        if not b: break
+#     time.sleep(0.1)
+
+#     # This essentially allows you to debug the serial comms!
+#     # (ampy run won't work for debugging serial since it requires full use of the serial port itself)
+#     while(True):
+#         b = ser.readline()
+#         print(b)
+#         if not b: break
 
 #b = ser.read(4)
 
