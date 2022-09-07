a13536e4837c988e4cb86635d477516575c001ac
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2022-09-07 19:50:29 +0100

    Final tweaks to SoundCard test

--- src/test/host/vu_meter_sc.py
+++ src/test/host/vu_meter_sc.py
@@ -1,12 +1,14 @@
 import soundcard as sc
 import numpy as np
+import sys
 
 #mic = sc.default_microphone()
 
 # This almost seems *too* easy...
 mic = sc.all_microphones(include_loopback = True)[0]
 
-with mic.recorder(samplerate = 48000) as mic:
-    for _ in range(500):
-        data = mic.record(numframes=1024)
-        print("#" * (int(np.max(data) * 500)))
+with mic.recorder(samplerate = 48000) as rec:
+    while True:
+        data = rec.record(numframes=1024)
+        sys.stdout.write("\033[K")
+        print("#" * (int(np.max(data) * 500)), end = "\r")
