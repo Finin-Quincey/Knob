f380eb2279ee050836776a62ccec6ce6f951c686
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2023-05-28 12:08:52 +0100

    System tray icon setup

--- src/main/main.py
+++ src/main/main.py
@@ -1,1 +1,31 @@
-from host import host_controller
+import pystray
+
+from PIL import Image
+
+image = Image.open("src/main/systray_icon.png")
+
+menu = pystray.Menu(
+    pystray.MenuItem(
+        text = "Open Settings",
+        action = lambda icon, item: print(f"{item} clicked")
+    ),
+    pystray.MenuItem(
+        text = "View Logs",
+        action = lambda icon, item: print(f"{item} clicked")
+    ),
+    pystray.MenuItem(
+        text = "Restart",
+        action = lambda icon, item: print(f"{item} clicked")
+    ),
+    pystray.MenuItem(
+        text = "Quit",
+        action = lambda icon, item: print(f"{item} clicked")
+    )
+)
+
+icon = pystray.Icon("volume_knob", icon = image, title = "Knob", menu = menu, visible = True)
+
+icon.run()
+
+def run_host_process():
+    from host import host_controller
