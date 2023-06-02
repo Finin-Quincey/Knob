076e98355f0d85d76dfa50218e50b534f35f02e9
Finin-Quincey <57540995+Finin-Quincey@users.noreply.github.com>
2023-06-02 16:30:04 +0100

    More menu items

--- src/main/main.py
+++ src/main/main.py
@@ -1,3 +1,10 @@
+"""
+main.py
+
+Host code entry point. Responsible for the system tray icon/menu, as well as integrating with the rest of
+the program.
+"""
+
 import pystray
 
 from PIL import Image
@@ -6,13 +13,26 @@ image = Image.open("src/main/systray_icon.png")
 
 menu = pystray.Menu(
     pystray.MenuItem(
-        text = "Open Settings",
+        text = "Enable Lighting",
+        action = lambda icon, item: print(f"{item} clicked"),
+        checked = lambda item: True,
+        default = True
+    ),
+    pystray.MenuItem(
+        text = "Lock to Current App",
+        action = lambda icon, item: print(f"{item} clicked"),
+        checked = lambda item: True
+    ),
+    pystray.Menu.SEPARATOR,
+    pystray.MenuItem(
+        text = "Open Settings File",
         action = lambda icon, item: print(f"{item} clicked")
     ),
     pystray.MenuItem(
         text = "View Logs",
         action = lambda icon, item: print(f"{item} clicked")
     ),
+    pystray.Menu.SEPARATOR,
     pystray.MenuItem(
         text = "Restart",
         action = lambda icon, item: print(f"{item} clicked")
